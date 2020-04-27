14b9817cc972d5f109355472378c4e807dd92668
Laurent Gatto <laurent.gatto@uclouvain.be>
2020-04-27 15:52:31 +0200

    add vis/proc section

--- S03-vis.R
+++ S03-vis.R
@@ -11,30 +11,25 @@ serine_mz <- 106.049871
 mzr <- c(serine_mz - 0.01, serine_mz + 0.01)
 rtr <- c(175, 187)
 
-## Filtering the object
+## Raw MS profile
 serine <- data_prof %>%
     filterRt(rtr) %>%
     filterMz(mzr)
 
-
-data_sg <- data_prof %>%
-    smooth(method = "SavitzkyGolay", halfWindowSize = 4L)
-
-
-## Use pickPeaks with descendPeak m/z refinement
-data_sg_cent_mz <- data_sg %>%
-    pickPeaks(refineMz = "descendPeak")
-
-serine_sg_cent_mz <- data_sg_cent_mz %>%
+## Processing
+serine_proc <- data_prof %>%
+    smooth(method = "SavitzkyGolay", halfWindowSize = 4L) %>% 
+    pickPeaks(refineMz = "descendPeak") %>%
     filterRt(rtr) %>%
     filterMz(mzr)
 
 
-pdf("./figure/centroiding.pdf", width = 12, height = 7)
+pdf("./figure/centroiding.pdf", width = 9, height = 6)
 layout(matrix(1:4, ncol = 2))
 plot(serine, type = "XIC", layout = NULL)
 abline(h = serine_mz, col = "red", lty = 2)
-plot(serine_sg_cent_mz, type = "XIC", layout = NULL)
+plot(serine_proc, type = "XIC", layout = NULL)
 abline(h = serine_mz, col = "red", lty = 2)
 abline(v = rtime(serine_sg_cent_mz)[22], col = "red", lty = 3)
 dev.off()
+
